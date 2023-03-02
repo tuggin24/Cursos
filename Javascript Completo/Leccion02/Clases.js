cb4567c48377fca776fcc69e510b2cde8696d055
tuggin24 <agustinloko74@gmail.com>
2023-03-01 20:42:36 -0500

    objetos y clases

---
+++ Javascript Completo/Leccion02/Clases.js
@@ -0,0 +1,13 @@
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+}
+
+//objeto o instancia de clase
+let persona1 = new Persona('Agustin','Salazar');
+console.log(persona1);
+
+let persona2 = new Persona('Pepe','Pecas');
+console.log(persona2);
--- Javascript Completo/Leccion02/Objetos.js
+++ Javascript Completo/Leccion02/Objetos.js
@@ -91,3 +91,57 @@ console.log(persona.lang);
 persona.setlang = 'en';
 console.log(persona.lang);
 console.log(persona.idioma);
+
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = new String('HOLA');//Esta es mas formal
+let miCadena2 = 'HOLA';//Es mas recomendable por ser mas corta
+
+
+//Metodo constructor se usa para reutilizar objetos y no crearlos uno por uno
+function Persona(nombre,apellido,email){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
+}
+
+//Uso de propotype agrega el atrubuto o metodo a todos los nuevos objetos
+Persona.prototype.tel = '123123';
+
+let nuevaPersona = new Persona('Agustin','Andres','aasalazar@gmail.com');
+console.log(nuevaPersona.tel);
+console.log(nuevaPersona,nuevaPersona.nombreCompleto());
+
+let nuevaPersona2 = new Persona('Andres','Salazar','asalazar@gmail.com');
+console.log(nuevaPersona2,nuevaPersona2.nombreCompleto());
+
+//Uso de call nos permite llamar un metodo de un objeto desde otro objeto
+let persona1 = {
+    nombre : 'Juan',
+    apellido : 'Perez',
+    nombreCompleto : function(titulo,tel){
+        //return this.nombre + ' ' + this.apellido;
+        return titulo + ': ' + this.nombre + ' ' + this.apellido + ' ' + tel;
+    }
+};
+
+let persona3 = {
+    nombre : 'Carlos',
+    apellido : 'Lara'
+};
+
+//Uso de call opara usar el metodo persona1.nombreCompleto con los datos del objeto persona2
+console.log(persona1.nombreCompleto());
+console.log(persona1.nombreCompleto.call(persona3));
+//Se puede enviar argumentos al call
+console.log(persona1.nombreCompleto.call(persona3,'Inge',3106961132));
+
+//Metodo apply se usa para llamar metodos de otros objetos como el call
+//Se pueden enviar argumentos
+let arreglo = ['Lic',3106987];
+console.log(persona1.nombreCompleto.apply(persona3,arreglo));
+console.log(persona1.nombreCompleto.apply(persona3,['Dr.',3106987123]));
